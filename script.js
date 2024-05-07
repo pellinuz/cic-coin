048b4b46e07c356ebe180b6997e4f417c38288d7
pellinuz <78985892+pellinuz@users.noreply.github.com>
2024-05-07 14:00:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var euro = document.getElementById("euro");
+var cicCoin = document.getElementById("ciccoin")
+var cicValue = "69,104"
+
+function changeValue() {
+	var changed = euro.value * cicValue;
+	cicCoin.innerHTML = `<p>` + changed + `</p>`;
+}
